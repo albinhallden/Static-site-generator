9cb1afd03969d83eb91072adbf1d83470d9f3c53
Albin Hallden <albin.hallden@cybercom.com>
2016-06-10 07:32:50 +0200

    Add front matter support and add default directories for nunjucks

--- index.js
+++ index.js
@@ -2,39 +2,46 @@ import path from 'path';
 import fs from 'fs';
 import nunjucks from 'nunjucks';
 import mkdirp from 'mkdirp';
+import fm from 'front-matter';
+
+const nunjucksEnv = new nunjucks.Environment(
+  new nunjucks.FileSystemLoader(['./src/views/'])
+);
 
 const baseDir = path.join(process.cwd(), './src/views/pages/');
 const distDir = path.join(process.cwd(), './dist/');
 
+const writeFile = (relativePath) => {
+  const absolutePath = path.join(baseDir, relativePath);
+  const directory = relativePath.substring(0, relativePath.lastIndexOf('\\'));
+  const fileName = relativePath.substring(relativePath.lastIndexOf('\\') + 1);
+
+  const fmData = fm(fs.readFileSync(absolutePath, { encoding: 'utf8' }));
+  const rendered = nunjucksEnv.render(absolutePath, fmData.attributes);
+
+  const distPath = path.join(distDir, directory);
+  mkdirp(distPath, (err) => {
+    if (err) {
+      console.log('Error creating:', directory); // eslint-disable-line
+    } else {
+      const filePath = path.join(distPath, fileName);
+      fs.writeFileSync(filePath, rendered);
+      console.log('Write', filePath); // eslint-disable-line
+    }
+  });
+}
+
 // Collect all views and render them
 const getFileNames = (dir = '/') => {
-  let filePaths = [];
   const directory = path.join(baseDir, dir);
   fs.readdirSync(directory).forEach(currentPath => {
     const absolutePath = path.join(directory, currentPath);
     const relativePath = path.join(dir, currentPath);
     if (fs.statSync(absolutePath).isDirectory()) {
-      filePaths = filePaths.concat(getFileNames(relativePath));
+      getFileNames(relativePath);
     } else {
-      filePaths.push(relativePath);
+      writeFile(relativePath);
     }
   });
-  return filePaths;
 }
-
-getFileNames().forEach(templatePath => {
-  const absolutePath = path.join(baseDir, templatePath);
-  const directory = templatePath.substring(0, templatePath.lastIndexOf('\\'));
-  const fileName = templatePath.substring(templatePath.lastIndexOf('\\') + 1);
-  const rendered = nunjucks.render(absolutePath);
-
-  const distPath = path.join(distDir, directory);
-  mkdirp(distPath, (err) => {
-    if (err) {
-      console.log('error creating:', directory); // eslint-disable-line
-    } else {
-      fs.writeFileSync(path.join(distPath, fileName), rendered);
-      console.log('write file', directory); // eslint-disable-line
-    }
-  });
-});
+getFileNames();
